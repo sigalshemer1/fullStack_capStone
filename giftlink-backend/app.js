ab30dac5c5ac3a1b93b08dcdb68271eae83f973b
sigalshemer1 <sigal.shemer68@gmail.com>
2024-07-31 05:45:25 -0400

    Complete commit message here

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -28,7 +28,7 @@ app.use('/api/gifts', giftRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
-
+app.use('/api/search', searchRoutes);
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -9,18 +9,14 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-
-    // Task 1: Connect to MongoDB
-    const client = new MongoClient(url); 
-    await client.connect();
-    
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    dbInstance = client.db(dbName);
-   
-    // Task 3: Return database instance
      if (dbInstance){
         return dbInstance
     };
+
+    const client = new MongoClient(url);
+    await client.connect();
+    dbInstance = client.db(dbName);
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
