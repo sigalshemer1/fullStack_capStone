bb9224b9b71ec1bad0659ac78727d82284694edb
sigalshemer1 <sigal.shemer68@gmail.com>
2024-07-31 05:28:18 -0400

    changes to final project

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -9,20 +9,18 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
-
-    const client = new MongoClient(url);      
 
     // Task 1: Connect to MongoDB
-    // {{insert code}}
-
+    const client = new MongoClient(url); 
+    await client.connect();
+    
     // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
+    dbInstance = client.db(dbName);
+   
     // Task 3: Return database instance
-    // {{insert code}}
+     if (dbInstance){
+        return dbInstance
+    };
 }
 
 module.exports = connectToDatabase;
